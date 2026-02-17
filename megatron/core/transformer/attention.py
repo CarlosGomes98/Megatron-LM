8e7e5b7805607aa8533abc9ed107baa2cc1d66ae
CarlosGomes98 <carlosmiguel.gomes@live.com.pt>
2026-02-17 13:51:39 +0100

    Revert changes to attention.py

--- megatron/core/transformer/attention.py
+++ megatron/core/transformer/attention.py
@@ -55,9 +55,7 @@ except ImportError:
     rearrange = None
 
 try:
-    from flash_attn_3.flash_attn_interface import (
-        _flash_attn_forward,
-    )
+    from flash_attn_3.flash_attn_interface import _flash_attn_forward
     from flash_attn_3.flash_attn_interface import (
         flash_attn_with_kvcache as flash_attn3_with_kvcache,
     )
@@ -68,9 +66,7 @@ except ImportError as e:
 
 if not HAVE_FA3:
     try:
-        from flashattn_hopper.flash_attn_interface import (
-            _flash_attn_forward,
-        )
+        from flashattn_hopper.flash_attn_interface import _flash_attn_forward
         from flashattn_hopper.flash_attn_interface import (
             flash_attn_with_kvcache as flash_attn3_with_kvcache,
         )
